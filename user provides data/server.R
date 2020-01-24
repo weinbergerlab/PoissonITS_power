7d96f4e48e7221a8e1fc1fd4b46a8063186cfd6f
weinbergerlab <dweinber@gmail.com>
2020-01-23 22:43:04 -0500

    mse function

--- user provides data/server.R
+++ user provides data/server.R
@@ -15,6 +15,47 @@ power.month.trim<-lapply(rr.ests.compile.trim, function(x){
 })
 power.month.trim.array<-do.call('cbind', power.month.trim)
 
+dateFormats = list(
+  `YYYY-MM-DD`="%Y-%m-%d",
+  `YYYY-DD-MM`="%Y-%d-%m",
+  `MM-DD-YYYY`="%m-%d-%Y",
+  `DD-MM-YYYY`="%d-%m-%Y"
+)
+
+# TRUE if format is valid date format for v
+# Format is valid if the vector can be converted to dates in that format and if differences between nearby dates are >5
+validFormat = function(v, format) {
+  dates = as.Date(as.character(v), format)
+  if(all(!is.na(dates))) {
+    # This gets all the deltas between distinct adjacent dates
+    diffs = dates %>% sort() %>% unique() %>% diff() %>% unique()
+    # Of which none should be <20 or >200, because we're expecting monthly or quarterly data
+    all(diffs > 20 & diffs < 200)
+  } else {
+    FALSE
+  }
+}
+
+# Auto-detect viable time columns and their formats. Empty list if none are found, NULL if data is NULL
+dateColumns = function(data) {
+  names(data) %>% 
+    sapply(function(name) {
+      # List of viable formats for named column; NULL if none
+      dateFormats %>% 
+        lapply(function(format) {
+          if(validFormat(data[[name]], format)) {
+            format
+          }
+        }) %>% 
+        compact() %>%
+        (function(x) {
+          if (length(x) > 0) {
+            x
+          }
+        })
+    }, simplify = FALSE, USE.NAMES = TRUE) %>%
+    compact() 
+}
 
 
 shinyServer(function(input, output, clientData, session) {
